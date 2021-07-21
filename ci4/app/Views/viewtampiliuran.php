cec21016ff30aa31cbee6fc8caa17e84e38e0bf2
suriapratama0 <56243275+suriapratama0@users.noreply.github.com>
2021-07-21 15:37:55 +0700

    Iuran_KAS_RT

--- ci4/app/Views/viewtampiliuran.php
+++ ci4/app/Views/viewtampiliuran.php
@@ -12,8 +12,9 @@
 		td{
 			text-align: center;
 		}
-		button{
+		.hapus button{
 			margin: 10px 10px;
+			border: 1px solid black;
 		}
 		thead{
 			background-color: lightblue;
@@ -23,21 +24,60 @@
 		table{
 			background-color: lightgray;
 			border-color: black;
+		}		
+		footer{
+			width: 100%;
+			height: 40px;
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			background: #123;
+			color: #ffffff;
+			font-size: 1em;
+			text-transform: uppercase;
+			z-index: 1;
+			bottom: 0px;	
 		}
-		button{
-			border: 1px solid black;
+		ul{
+			list-style-type: none;
+			margin: 0;
+			padding: 0;
+			overflow: hidden;
+			background-color: #123;
+		}
+		li{
+			float: left;
+		}
+		li button {
+			display: block;
+			color: white;
+			text-align: center;
+			padding: 14px 16px;
+			text-decoration: none;
+			font-family: "Times New Roman", Times, serif;
+			font-size: 20px;
+		}
+		li button:hover {
+			background-color: lightblue;
+		}
+		.header{
+			background-color: #123;
+			border-color: lightgrey;
 		}
 	</style>
 </head>
 <body>
 	<h2>Data Iuran Warga</h2>
-	<button type="button" onclick="window.location='<?php echo site_url('warga/index') ?>'">
-		Kembali
-	</button>
-	<button type="button" onclick="window.location='<?php echo site_url('Iuran/formtambah') ?>'">
-		Tambah Data Iuran
-	</button>
-	<br>
+	<nav>
+		<ul>
+		<li><button class="header" type="button" onclick="window.location='<?php echo site_url('warga/index') ?>'">
+			Kembali
+		</button></li>
+		<li><button class="header" type="button" onclick="window.location='<?php echo site_url('Iuran/formtambah') ?>'">
+			Tambah Data Iuran
+		</button></li>
+		<ul>
+	</nav>
 	<br>
 	<table border="1">
 		<thead>
@@ -53,7 +93,7 @@
 				<th>Aksi</th>
 			</tr>
 		</thead>
-		<tbody>
+		<tbody class="hapus">
 			<?php 
 			$nomor = 0;
 			foreach ($tampildata as $row):
@@ -72,7 +112,6 @@
 						<button type="button" onclick="hapus('<?= $row->warga_id ?>')">
 							Hapus
 						</button>
-						
 					</td>
 				</tr>
 				<?php  
@@ -91,3 +130,6 @@
 	</script>
 </body>
 </html>
+<footer>         
+		<p>&copy; 2021 - Suria Pratama - 311910113 - TI.19.A.1 - Universita Pelita Bangsa.</p>     
+</footer>
